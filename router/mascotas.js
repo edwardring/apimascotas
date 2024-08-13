99221924ec39b92d4413578fa870c2f0bacd1101
edwardring <edward.rodriguez@gmail.com>
2024-08-13 11:54:42 -0500

    CRUD Mascotas

--- router/mascotas.js
+++ router/mascotas.js
@@ -13,4 +13,40 @@ router.post("/mascotas", (req, res) => {
         .catch((error) => res.json({message: error }));
 })
 
+/* consulta de mascotas */
+router.get("/mascotas", (req, res) => {
+    Mascota
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+})
+
+/* consultar una mascota */
+router.get("/mascotas/:id", (req, res) => {
+    const { id } = req.params;
+    Mascota
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+/* actualizar una mascota */
+router.put("/mascotas/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, descripcion } = req.body;
+    Mascota
+        .updateOne({ _id: id }, { $set: {nombre, descripcion}})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+/* borrar una mascota */
+router.delete("/mascotas/:id", (req, res) => {
+    const { id } = req.params;
+    Mascota
+        .findByIdAndDelete({ _id: id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+})
+
 module.exports = router;
